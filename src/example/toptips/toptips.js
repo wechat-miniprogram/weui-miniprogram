23bb38a8337821a7f202726be0b93ade638e559e
Tidyzq <tidyzq@tidyzq.com>
2021-11-23 21:39:36 +0800

    feat(toptips): update example

--- src/example/toptips/toptips.js
+++ src/example/toptips/toptips.js
@@ -2,11 +2,23 @@ import CustomPage from '../../base/CustomPage'
 
 CustomPage({
     data: {
-        show: false
+        show1: false,
+        show2: false,
+        show3: false
     },
-    showToptips() {
+    showToptips1() {
         this.setData({
-            show: true
+            show1: true
+        })
+    },
+    showToptips2() {
+        this.setData({
+            show2: true
+        })
+    },
+    showToptips3() {
+        this.setData({
+            show3: true
         })
     }
 })
