208fff090e59d888b92bde63be64799bcce91bb5
BitterGourd <bittergourd1020@gmail.com>
2024-07-09 21:19:00 +0800

    fix: package-lock.json and style or interactive bug

--- src/example/gallery/gallery.js
+++ src/example/gallery/gallery.js
@@ -3,9 +3,9 @@ import CustomPage from '../../base/CustomPage'
 CustomPage({
     data: {
         imgUrls: [
-            'http://desk-fd.zol-img.com.cn/g5/M00/02/05/ChMkJ1bKyZmIWCwZABEwe5zfvyMAALIQABa1z4AETCT730.jpg',
-            'http://desk-fd.zol-img.com.cn/g5/M00/02/05/ChMkJ1bKyZmIWCwZABEwe5zfvyMAALIQABa1z4AETCT730.jpg',
-            'http://desk-fd.zol-img.com.cn/g5/M00/02/05/ChMkJ1bKyZmIWCwZABEwe5zfvyMAALIQABa1z4AETCT730.jpg'
+            'https://res.wx.qq.com/a/wx_fed/assets/res/OTE0YTAw.png',
+            'https://res.wx.qq.com/a/wx_fed/assets/res/OTE0YTAw.png',
+            'https://res.wx.qq.com/a/wx_fed/assets/res/OTE0YTAw.png',
         ],
         show: true
     },
